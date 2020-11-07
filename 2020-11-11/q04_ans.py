ee8c4f7e83828c7cfa6dbba009e6153a216502bc
kogepanh <k.hosono1209@gmail.com>
2020-11-07 18:37:33 +0900

    edit

--- 2020-11-11/q04_ans.py
+++ 2020-11-11/q04_ans.py
@@ -1,13 +1,13 @@
-# Q04
+# Q04 Answer
 
 def cutbar(n, m):
     num = 1     # 棒の数
     count = 0   # 切った回数
 
     # [n]cmの棒を[num]本になるまで分割する
-    while *** :
+    while num < n:
         # 棒の本数が人数より多ければ，棒の数が人数分増える．棒の数の方が多ければ，棒の数が倍増する．
-        # *************
+        num += m if num > m else num
         count += 1
 
     return count
