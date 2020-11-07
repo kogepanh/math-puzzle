7ef852af080ce292fc5757f55087b23eefa78e1c
kogepanh <k.hosono1209@gmail.com>
2020-11-07 21:15:10 +0900

    edit

---
+++ 2020-11-11/q17.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+    if( n < 2 ) return n;
+    else return fib(n-1) + fib(n-2);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << "=> Pattern: " << fib(n+2) << endl;
+}
